0ed54e19cd0429576a93cc173807a5472f639c1e
Mohammed Al-Jabali <mohammed.aljablai@gmail.com>
2026-08-24 18:21:32 +0300

    chore: add `.gitattributes` and update config and package, in Addition Add Shadcn

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
-module.exports = {
-  darkMode: ['class'],
+/** @type {import('tailwindcss').Config} */
+const config = {
+  darkMode: ["class"],
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx}',
-    './src/components/**/*.{js,ts,jsx,tsx}',
-    './src/app/**/*.{js,ts,jsx,tsx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
-    extend: {},
-  },
-  plugins: [require('tailwindcss-animate')],
-}
+};
+
+export default config;
